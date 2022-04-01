e4e522ca28f0e9fa61e2df54b8f46d6856e3425c
pcurl1986 <101307058+pcurl1986@users.noreply.github.com>
2022-03-31 21:02:30 -0700

    Add files via upload

---
+++ pypoll.py
@@ -0,0 +1,34 @@
+# The data we need to retrieve
+#1. The total number of votes cast
+#2. A complete list of candidates who received votes
+#3. The percentage of votes each candidate won
+#4. The total number of votes each candidate won
+#5. The winner of the election based on popular vote
+
+# Add our dependencies.
+import csv
+import os
+# Assign a variable to load a file from a path.
+file_to_load = os.path.join("Resources", "election_results.csv")
+# Assign a variable to save the file to a path.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+#total votes 
+
+total_votes = 0
+
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+    file_reader = csv.reader(election_data)
+
+    # Read the header row.
+    headers = next(file_reader)
+
+    # Print each row in the CSV file.
+    for row in file_reader:
+        #2. Add to the total vote count.
+        total_votes += 1
+
+#3. print the total votes
+
+print(total_votes)
